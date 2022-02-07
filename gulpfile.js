294ca9e384abccffa8b2d25cd1fde1c328c8eff2
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-07 18:49:32 +0100

    edited gulpfile.js file

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-  return src('*.scss')
+  return src('handsome/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['*.scss'], buildStyles)
+    watch(['handsome/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
